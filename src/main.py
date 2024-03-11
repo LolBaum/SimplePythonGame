b2dc38bf29cb038e3d2e48bb1317daafb067ba54
wurzel <og.og.og.plo@gmail.com>
2024-03-11 14:31:51 +0100

    ball

--- src/main.py
+++ src/main.py
@@ -12,14 +12,17 @@ if __name__ == "__main__":
     clock = pygame.time.Clock()
     font = pygame.font.Font(size=40)
 
-    b = Ball((200, 200), 20)
+    white_ball = Ball((200, 200), 20)
+    dragging = False
+    drag_start_pos = (0, 0)
+    dragging_pos = (0, 0)
+
+    bg_color = pygame.Color('black')
 
     print("Starting main loop")
     while True:
         pygame.display.flip()
         clock.tick(30)
-
-        b.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -33,5 +36,32 @@ if __name__ == "__main__":
                     sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == pygame.BUTTON_LEFT:
+                    print("dragging = ", dragging)
                     x, y = pygame.mouse.get_pos()
+                    if white_ball.rect.collidepoint(x, y):
+                        dragging = True
+                        drag_start_pos = white_ball.rect.center #(x, y)
+                        print("white_ball")
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == pygame.BUTTON_LEFT:
+                    dragging = False
+                    print("dragging = ", dragging)
+            if dragging:
+                if not pygame.mouse.get_pressed(num_buttons=3)[0]:  # left mouse button
+                    dragging = False
+
+        screen.fill(bg_color)
+
+
+        white_ball.draw(screen)
+
+
+        if dragging:
+            print("dragging")
+            dragging_pos = pygame.mouse.get_pos()
+            pygame.draw.line(screen, (255, 255, 255), drag_start_pos, dragging_pos)
+
+
+
+
 
--- src/object.py
+++ src/object.py
@@ -1,5 +1,10 @@
 import pygame
-
+import numpy as np
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+       return v
+    return v / norm
 
 class Obj:
     def __init__(self, rect: pygame.rect.Rect,
@@ -14,6 +19,9 @@ class Obj:
     def draw(self, display: pygame.surface.Surface):
         display.blit(self.surf, self.rect)
 
+    def update(self):
+        pass
+
 
 class Ball(Obj):
     def __init__(self, pos: (int, int),
@@ -21,8 +29,13 @@ class Ball(Obj):
                  surf: pygame.surface.Surface = None):
         self.radius = radius
         size = radius*2
-        surf = pygame.surface.Surface((size, size))
+        surf = pygame.surface.Surface((size, size), pygame.SRCALPHA)
         pygame.draw.circle(surf, (200, 100, 100), (radius, radius), radius)
         super().__init__(pygame.rect.Rect(pos, (size, size)), surf)
+        self.vel = np.zeros(2)
+
+    def update(self):
+        pass
+
 
 
